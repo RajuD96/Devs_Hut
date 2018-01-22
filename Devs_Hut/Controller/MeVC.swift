de9abcaf1c10adb49bb3de399529e786aa80d06c
RD <rajudhumne@gmail.com>
2018-01-22 15:26:49 +0530

    added meVC and added UItabButton

---
+++ Devs_Hut/Controller/MeVC.swift
@@ -0,0 +1,28 @@
+//
+//  MeVC.swift
+//  Devs_Hut
+//
+//  Created by Raju Dhumne on 22/01/18.
+//  Copyright © 2018 Raju Dhumne. All rights reserved.
+//
+
+import UIKit
+
+class MeVC: UIViewController {
+
+    @IBOutlet weak var userImg: UIImageView!
+    @IBOutlet weak var emailLbl: UILabel!
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func signOutBtnWasPressed(_ sender: Any) {
+    }
+    
+
+}
